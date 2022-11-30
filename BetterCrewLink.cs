92897dc22e4f3d6c115d5a50c76969c9e9455920
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2022-11-30 14:38:27 -0700

    Basic working on AspNetCore

---
+++ BetterCrewLink.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Reactor.Networking.Attributes;
+
+namespace BCLDLL
+{
+    [BepInPlugin(PluginGUID, "BetterCrewLink API DLL", VersionString)]
+    [BepInProcess("Among Us.exe")]
+    [ReactorModFlags(Reactor.Networking.ModFlags.None)]
+    public class BetterCrewLink : BasePlugin
+    {
+        private const string VersionString = "0.0.1";
+        private const string PluginGUID = "com.essence.BCLDLL";
+        private static readonly Harmony harmony = new Harmony(PluginGUID);
+
+        public static ManualLogSource Logger;
+
+        public override void Load()
+        {
+            Logger = Log;
+            API api = new API();
+            Logger.LogDebug("API is loaded:" + api.loaded.ToString());
+            harmony.PatchAll();
+        }
+    }
+}
